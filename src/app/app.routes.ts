bbfa272124d0265da45fb5e18dd9e5a2da48721b
Yury Silva <yurysilva.dev@hotmail.com.br>
2024-09-25 23:22:37 -0300

    feat: cria rota default para o projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { TemplateComponent } from './core/components/template/template.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TemplateComponent,
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./feature/home/home.component').then(m => m.HomeComponent)
+            }
+        ]
+    }
+];
