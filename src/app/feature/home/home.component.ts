b9d8618a393c5490e62b76cd12491eb22bad7efb
Yury Silva <yurysilva.dev@hotmail.com.br>
2024-09-27 01:41:11 -0300

    feat: adiciona listagem, paginação e o pokemon card ao home

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,28 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { PokemonService } from '../../services/pokemon.service';
-import { Pokemon } from '../../shared/classes/pokemon.class';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BaseClass } from '../../shared/classes/base.class';
 import { SearchComponent } from '../../shared/components/search/search.component';
+import { PokemonCardComponent } from '../../shared/components/pokemon-card/pokemon-card.component';
+import { Pokemon } from '../../shared/classes/pokemon.class';
+import { finalize, forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { PokemonService } from '../../services/API/pokemon.service';
+import { PokemonSubjectService } from '../../services/Subjects/pokemon-subject.service';
+import { ModalPokemonComponent } from './components/modal-pokemon/modal-pokemon.component';
+import { PageWithPaginationComponent } from '../../shared/components/page-with-pagination/page-with-pagination.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, PokemonCardComponent, ModalPokemonComponent, CommonModule, PageWithPaginationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  pokemons!: Pokemon[];
-  searchPokemons!: Pokemon[];
-  constructor(private PokemonService: PokemonService) { }  
+  @ViewChild('modalPokemonDetails') modalPokemonDetails!: ModalPokemonComponent;
+  pokemonsList!: BaseClass[];
+  listPokemonsSearch: Pokemon[] = [];
+  isSearched: boolean = false;
+  page: number = 1;
+  pageSize: number = 10;
+  collectionSize: number = 0;
+
+  constructor(private PokemonService: PokemonService, private PokemonSubjectService: PokemonSubjectService) { }
 
   ngOnInit(): void {
-      this.PokemonService.getAllPokemons().subscribe({
-        next: (res) => this.pokemons = res,
-        error: (err) => console.log(err)
-      })
+    this.getAllNamesPokemons();
+  }
+
+  getAllNamesPokemons(): void {
+    this.PokemonService.getAllPokemons().subscribe({
+      next: (res) => {
+        this.pokemonsList = res
+        this.PokemonSubjectService.setState({ pokemonsList: this.pokemonsList })
+        this.collectionSize = this.pokemonsList.length
+        this.searchPagination(this.page)
+      },
+      error: (err) => console.log(err)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.PokemonSubjectService.resetState();
+  }
+  
+  onPageChange(event: any) {
+    this.searchPagination(event);
   }
 
   onSearch(search: string): void {
-    this.searchPokemons = this.pokemons.filter((p) => p.getName().includes(search));
+    if (search === '') {
+      this.collectionSize = this.pokemonsList.length;
+      return this.searchPagination(this.page);
+    }
+
+    const pokemonsListUrlSearch = this.pokemonsList.filter((p) => p.getName().includes(search));
+    this.collectionSize = pokemonsListUrlSearch.length;
+    this.page = 1;
+
+    if (pokemonsListUrlSearch.length === 0 && search !== '') {
+      this.listPokemonsSearch = [];
+      return;
+    }
+
+    this.searchPagination(this.page, pokemonsListUrlSearch);
+  }
+
+  searchPagination(page: number = 1, listBase: BaseClass[] = this.pokemonsList) {
+    const pokemonsListUrlSearch = [];
+    
+    for (let i = (page - 1) * this.pageSize; i < page * this.pageSize && i < listBase.length; i++) {
+      pokemonsListUrlSearch.push(this.PokemonService.getPokemon(listBase[i].getUrl()));
+    }
+
+    forkJoin(pokemonsListUrlSearch).pipe(
+      finalize(() => {
+        if (!this.isSearched) this.isSearched = true;
+      })
+    ).subscribe({
+      next: (res) => this.listPokemonsSearch = res,
+      error: (err) => console.log(err)
+    })
+  }
+
+  seeMoreDetailsEvent(): void {
+    this.modalPokemonDetails.open();
   }
 }
